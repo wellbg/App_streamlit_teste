90be2c05e5cee398604dc15d62c98db62570ac8f
Wellington Gonçalves <63022593+wellbg@users.noreply.github.com>
2023-05-19 21:33:23 -0300

    Update teste_page.py

--- teste_page.py
+++ teste_page.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 st.write("Testando página")
 
-icon_size = 20
 
-st.button('App_well', 'https://wellbg-app-streamlit-teste-app-streamlit-teste-w3kbfg.streamlit.app/','App', icon_size)
+st.button('App_well', 'https://wellbg-app-streamlit-teste-app-streamlit-teste-w3kbfg.streamlit.app/','App')
