fb9a8530bbb5bff847fd5aa57592ba58294e4789
Wellington Gonçalves <63022593+wellbg@users.noreply.github.com>
2023-05-19 21:34:51 -0300

    Update teste_page.py

--- teste_page.py
+++ teste_page.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.write("Testando página")
 
 
-st.button('App_well', 'https://wellbg-app-streamlit-teste-app-streamlit-teste-w3kbfg.streamlit.app/','App')
+st.button('App_well', 'https://wellbg-app-streamlit-teste-app-streamlit-teste-w3kbfg.streamlit.app/')
